ab7361a2d11209c13748abb8d96a6ff45b620da9
dnehdngs <wosm5798@naver.com>
2026-01-09 15:18:30 +0900

    owo

--- booknaraPrj/src/main/java/com/booknara/booknaraPrj/admin/settings/AdminSettings.java
+++ booknaraPrj/src/main/java/com/booknara/booknaraPrj/admin/settings/AdminSettings.java
@@ -2,9 +2,12 @@ package com.booknara.booknaraPrj.admin.settings;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Table(name = "SETTINGS")
 public class AdminSettings {
     @Id

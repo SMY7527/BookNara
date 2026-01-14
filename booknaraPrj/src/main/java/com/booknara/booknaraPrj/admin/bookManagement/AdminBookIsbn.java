746e9f81ee45a2fb78580e09d31d6fb11bd92f6d
dnehdngs <wosm5798@naver.com>
2026-01-15 02:52:49 +0900

    보안 관련 문제

--- booknaraPrj/src/main/java/com/booknara/booknaraPrj/admin/bookManagement/AdminBookIsbn.java
+++ booknaraPrj/src/main/java/com/booknara/booknaraPrj/admin/bookManagement/AdminBookIsbn.java
@@ -17,9 +17,7 @@ import java.util.List;
 @Builder
 @Table(name = "BOOK_ISBN")
 public class AdminBookIsbn {
-    //o3o
-    //owo
-    //;;;
+    //그만
     @Id
     @Column(name = "ISBN13", length = 20)
     private String isbn13;
